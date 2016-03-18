68351a2b532e1bbbc5172d65721c1b3ca0d0f71c
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2016-03-18 16:44:16 +0300

    Перевод шапки на флекс и косметические правки кода

--- src/index.php
+++ src/index.php
@@ -11,6 +11,7 @@
 </head>
 <body class="global">
   <div class="page">
+    <?php /*
     <div class="page__header | page-header">
       <div href="#" class="page-header__item page-header__item--brand">
         <a href="#" class="page-header-brand"></a>
@@ -38,6 +39,38 @@
     <div class="page__slider"></div>
     <div class="page__main"></div>
     <div class="page__footer"></div>
+    */ ?>
+    <div class="page__header | page-header">
+      <div class="page-header__item">
+        <a href="#" class="page-header-brand"></a>
+      </div>
+      <div class="page-header__item">
+        <div class="page-header-contact">
+          <h2 class="page-header-contact__title">Отдел продаж</h2>
+          <div class="page-header-contact__text">
+            <a class="page-header-contact__link" href="tel:+79621662014">+7 (962) 166-20-14</a>
+          </div>
+        </div>
+      </div>
+      <div class="page-header__item">
+        <div class="page-header-contact">
+          <div class="page-header-contact__text">
+            <a class="page-header-contact__link" href="tel:+74932227220">+7 (4932) 22-72-20</a>
+          </div>
+          <div class="page-header-contact__text">
+            <a class="page-header-contact__link" href="tel:+79022421170">+7 (902) 242-11-70</a>
+          </div>
+        </div>
+      </div>
+      <div class="page-header__item">
+        <form class="page-header-search">
+          <input class="page-header-search__input" placeholder="Поиск">
+        </form>
+      </div>
+      <div class="page-header__item">
+        <a href="#" class="page-header-cart">199050 руб.</a>
+      </div>
+    </div>
   </div>
 	<script src="<?= $template; ?>/scripts.js"></script>
 </body>
